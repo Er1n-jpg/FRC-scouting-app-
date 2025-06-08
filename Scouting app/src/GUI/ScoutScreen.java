0e194917f3b07db507856806fdfee4062214dbd1
Erin <enguy4@ocdsb.ca>
2025-06-07 22:48:10 -0400

    Add files via upload

---
+++ Scouting app/src/GUI/ScoutScreen.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ScoutScreen extends JFrame {
+    public ScoutScreen(){
+       setBounds(100, 200, 1500, 900);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
