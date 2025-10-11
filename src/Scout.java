db83b6c6beef4593362d259a28d8f8826f9185b6
Erin <enguy4@ocdsb.ca>
2025-10-11 00:04:47 -0400

    Changed image error

--- src/Scout.java
+++ src/Scout.java
@@ -86,7 +86,7 @@ public Scout(){
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Image background = new ImageIcon("C://Users//enchi//.vscode//Scouting app//src//Images haha//title.png").getImage();
+        Image background = new ImageIcon("src/title.png").getImage();
         g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
     }
 }
